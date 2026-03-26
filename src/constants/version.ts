0316df85d342349d88b7805447b821c3d1b2c294
Rulo <Rulo@Rulos-MacBook-Air.local>
2026-03-26 12:38:26 -0300

    UI: Bumped version to V13.1

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -1,4 +1,5 @@
 export const APP_NAME = "Kiev Platform";
-export const APP_VERSION = "V12.9";
+export const APP_VERSION = "V13.1";
+
 export const APP_PROTOCOL = "Kiev Intelligence Protocol";
 export const APP_SAFETY = "Kiev Safety Protocol";
